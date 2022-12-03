2014fe2f325ac27acca28e0c56eaad8eb62c132f
Bullets-jyr <jyr@bullets.co.kr>
2022-12-03 18:37:45 +0900

    프로젝트 주식리스트 03

---
+++ StockRank/StockRankCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StockRankCollectionViewCell.swift
+//  StockRank
+//
+//  Created by bullets on 2022/12/03.
+//
+
+import UIKit
+
+class StockRankCollectionViewCell: UICollectionViewCell {
+    
+}
--- StockRank/StockRankViewController.swift
+++ StockRank/StockRankViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class StockRankViewController: UIViewController {
     
+    let stockList: [StockModel] = StockModel.list
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    // Data, Presentation, Layout
+    // Data - 어떤 데이터? -> stockList
+    // Presentation - 셀을 어떻게 표현?
+    // Layout - 셀을 어떻게 배치할꺼야?
+    // protocol -
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
+}
+
+extension StockRankViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stockList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath)
+        
+        return cell
+    }
+}
+
+extension StockRankViewController: UICollectionViewDelegateFlowLayout {
+    
 }
